1c486318c82d5735d855930981e54793103dbf70
Kumar <o.kanshuman@gmail.com>
2025-05-21 20:29:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Anshuman, Hi Anchal</p>
+        <p>Any file</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
